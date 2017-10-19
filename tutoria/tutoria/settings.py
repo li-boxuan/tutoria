d5cf7fc38620883ce784cbbc0024d02296976706
ZHANG Jiayao <zjohnson@connect.hku.hk>
2017-10-19 21:25:33 +0800

    add template dir

--- tutoria/tutoria/settings.py
+++ tutoria/tutoria/settings.py
@@ -54,8 +54,8 @@ ROOT_URLCONF = 'tutoria.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
-        'APP_DIRS': True,
+        'DIRS': [os.path.join('..', 'templates')],
+        'APP_DIRS': False,
         'OPTIONS': {
             'context_processors': [
                 'django.template.context_processors.debug',
--- tutoria/tutoria/urls.py
+++ tutoria/tutoria/urls.py
@@ -13,11 +13,12 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
+from __future__ import (absolute_import, print_function)
 from django.conf.urls import url
 from django.contrib import admin
 from . import views
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
-    url(r'^$', views.hello),
+    url(r'^admin/', admin.site.urls, name='admin'),
+    url(r'^$', views.IndexView.as_view(), name='homepage'),
 ]
