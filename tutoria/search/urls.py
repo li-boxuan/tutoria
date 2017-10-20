30f25a51fb44ad5cde94cf38502bba24e1cd88b9
ZHANG Jiayao <zjohnson@connect.hku.hk>
2017-10-21 00:53:48 +0800

    update search app

--- tutoria/search/urls.py
+++ tutoria/search/urls.py
@@ -2,7 +2,10 @@ from django.conf.urls import url
 
 from . import views
 
-#app_name = 'search'
+app_name = ['search']
+
 urlpatterns = [
+    url(r'result$', views.ResultView.as_view(), name='result'),
     url(r'^$', views.IndexView.as_view(), name='index'),
+    
 ]
--- tutoria/search/views.py
+++ tutoria/search/views.py
@@ -1,8 +1,15 @@
 from django.shortcuts import get_object_or_404, render
-from django.http import HttpResponseRedirect
+from django.http import HttpResponse, HttpResponseRedirect
 from django.urls import reverse
 from django.views import generic
+
+from account.models import (User, Tutor)
 
 class IndexView(generic.TemplateView):
     template_name = 'search.html'
     # context_object_name = 'index_context'
+
+class ResultView(generic.TemplateView):
+    # tutor_list = Tutor.objects()
+    template_name = 'result.html'
+    context_object_name = 'results'
