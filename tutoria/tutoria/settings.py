564dd3beb7f15170d7a4d6ada0457e7c5897c33e
Li Boxuan <bli@cplg104-mac25.cs.hku.hk>
2017-10-20 19:42:20 +0800

    modify static files dir setting

--- tutoria/tutoria/settings.py
+++ tutoria/tutoria/settings.py
@@ -124,6 +124,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 # root dir of static files
-STATIC_ROOT = os.path.abspath(os.path.join(BASE_DIR, 'static/'))
+#STATIC_ROOT = os.path.abspath(os.path.join(BASE_DIR, 'static/'))
+STATIC_ROOT = ''
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (os.path.join('static'),)
