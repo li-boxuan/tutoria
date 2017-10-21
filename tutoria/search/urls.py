0e556e76de217c06b83ab0d3ee56fe809fb5843a
ZHANG Jiayao <zjohnson@connect.hku.hk>
2017-10-21 21:05:20 +0800

    app_name should be a str

--- tutoria/search/urls.py
+++ tutoria/search/urls.py
@@ -2,11 +2,11 @@ from django.conf.urls import url
 
 from . import views
 
-app_name = ['search']
+app_name = 'search'
 
 urlpatterns = [
     # url(r'debug$', views.debug, name='debug'),
-    url(r'result$', views.ResultView.as_view(), name='result'),
+    url(r'result/$', views.ResultView.as_view(), name='result'),
     url(r'^$', views.IndexView.as_view(), name='index'),
     
 ]
