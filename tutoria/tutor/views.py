acb7564a4f44f49d0810511bf111a16954ba1135
Li Boxuan <bli@cplg104-mac25.cs.hku.hk>
2017-10-21 19:52:00 +0800

    [tutor] add urls.py and construct views.py

--- tutoria/tutor/views.py
+++ tutoria/tutor/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
+from django.views import generic
 
-# Create your views here.
+from account.models import (User, Tutor)
+
+class IndexView(generic.TemplateView):
+    template_name = 'result.html'
+    # context_object_name = 'index_context'
+
