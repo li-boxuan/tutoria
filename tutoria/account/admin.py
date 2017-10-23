20a3505ebd850f9434c02d74405c28c0bbae5f84
ZHANG Jiayao <zjohnson@connect.hku.hk>
2017-10-23 20:29:25 +0800

    add models to admin site

--- tutoria/account/admin.py
+++ tutoria/account/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from account.models import (User, Student, Tutor, SubjectTag, Course)
 
-# Register your models here.
+
+admin.site.register(User)
+admin.site.register(Student)
+admin.site.register(Tutor)
+admin.site.register(SubjectTag)
+admin.site.register(Course)
+
+
+
+
+
