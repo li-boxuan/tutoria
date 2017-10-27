15e329151295c3b61232f1c6a43ba045c251f4df
ZHANG Jiayao <zjohnson@connect.hku.hk>
2017-10-27 23:11:33 +0800

    updated db schema

--- tutoria/wallet/models.py
+++ tutoria/wallet/models.py
@@ -10,12 +10,13 @@ from django.db import models
 
 class Transaction(models.Model):
     """Models a transaction."""
-    issuer = models.ForeignKey('account.User', on_delete=models.CASCADE,
+    issuer = models.ForeignKey('account.Student', on_delete=models.CASCADE,
                               related_name='issuer')
-    receiver = models.ForeignKey('account.User', on_delete=models.CASCADE,
+    receiver = models.ForeignKey('account.Tutor', on_delete=models.CASCADE,
                                 related_name='receiver')
     amount = models.PositiveIntegerField()
     created_at = models.DateTimeField()
+    commission = models.FloatField()
 
     def __str__(self):
         return "{} -> {} :: ${}".format(issuer, receiver, amount)
@@ -24,7 +25,9 @@ class Transaction(models.Model):
 class Coupon(models.Model):
     """Models a coupon."""
     import uuid
+    start_data = models.DateTimeField()
+    end_data = models.DateTimeField()
     code = models.UUIDField(primary_key=True,
                            default=uuid.uuid4, editable=False)
-    record = models.ForeignKey('scheduler.BookingRecord')
+    transaction = models.ForeignKey(Transaction)
 
