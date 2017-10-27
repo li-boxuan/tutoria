3a88342d87bf6de631ab6a9fed19f60cff425bcc
ZHANG Jiayao <zjohnson@connect.hku.hk>
2017-10-27 23:59:44 +0800

    updated db population

--- tutoria/populate_db.py
+++ tutoria/populate_db.py
@@ -10,6 +10,8 @@ by Jiayao
 """
 import os
 import numpy as np
+from scheduler.models import Session
+
 
 def add_student(username, password, email, first_name, last_name,
                 wallet_balance=-1, avator='default_avator.png'):
@@ -26,21 +28,25 @@ def add_student(username, password, email, first_name, last_name,
     return student
 
 def add_tutor(username, password, email, first_name, last_name,
+                          tutor_type = 'CT',
                           hourly_rate=0,
                           bio='',
                           courses=[['COMP3297', 'Software Engineering']],
                             tags=['Software Engineering'],
-                          wallet_balance=-1, avator='default_avator.png'):
+                          wallet_balance=-1, avator='default_avator.png',
+                        sessions=None):
     from account.models import (Tutor, Course, SubjectTag)
     if wallet_balance < 0:
         wallet_balance = np.random.randint(1, 300) * 10
-    if hourly_rate <= 0:
+    if tutor_type == 'CT':
         hourly_rate = 0
-    tutor_type = 'CT' if hourly_rate == 0 else 'PT'
+    elif hourly_rate < 0:
+        hourly_rate = np.random.randint(1, 300) * 10
+
     tutor = Tutor.objects.create_user(username, email, password,
                                          first_name=first_name,
                                          last_name=last_name)
-    tutor_type=tutor_type
+    tutor.tutor_type = tutor_type
     tutor.wallet_balance = wallet_balance
     tutor.avator = avator
     tutor.hourly_rate = hourly_rate
@@ -51,6 +57,10 @@ def add_tutor(username, password, email, first_name, last_name,
         tutor.tags.add(add_tag(tag))
     for course in courses:
         tutor.courses.add(add_course(course[0], course[1]))
+    if sessions is not None:
+        for session in sessions:
+            session.tutor = tutor
+            session.save()
     tutor.save()
     return tutor
 
@@ -69,22 +79,22 @@ def add_course(code, name):
 def populate_tutor():
     tutors = []
     tutors.append(add_tutor(
-        'georgem', 'georgem', 'georgem@cs.hku.hk', 'George', 'Michetson', 0,
+        'georgem', 'georgem', 'georgem@cs.hku.hk', 'George', 'Michetson', 'CT', 0,
 r'Before joining HKU, George accumulated many years of experience in large-scale software engineering and in R&D for real-time systems. He has headed or contributed to development of a wide range of systems spanning fields such as scientific computation, telecommunications, database management systems, control systems and autonomous robotics. This work was carried out principally in Europe and the USA. Between the two he taught for several years at the University of Puerto Rico.',
         [['COMP3297', 'Software Engineering'],
-         ['COMP3???', 'Software Implmentation, Testing and Maintainence']],
+         ['COMP3403', 'Software Implmentation, Testing and Maintainence']],
         ['Software Engineering', 'Evolutionary Computing']
     ))
 
     tutors.append(add_tutor(
-        'clwang', 'choli', 'clwang@cs.hku.hk', 'Cho-Li', 'Wang', 0,
+        'clwang', 'choli', 'clwang@cs.hku.hk', 'Cho-Li', 'Wang', 'CT', 0,
 r"Professor Cho-Li Wang received his B.S. degree in Computer Science and Information Engineering from National Taiwan University in 1985. He obtained his M.S. and Ph.D. degrees in Computer Engineering from University of Southern California in 1990 and 1995 respectively. He is currently a professor at the Department of Computer Science. Professor Wang's research interests include parallel architecture, software systems for Cluster and Grid computing, and virtualization techniques for Cloud computing. Recently, he starts working on software transaction memory for multicore/GPU clusters and multi-kernel operating systems for future single-chip manycore processor. Professor Wang has published more than 130 papers in various peer reviewed journals and conference proceedings. He is/was on the editorial boards of several international journals, including IEEE Transactions on Computers (TC), Multiagent and Grid Systems (MGS), Journal of Information Science and Engineering (JISE), International Journal of Pervasive Computing and Communications (JPCC), ICST Transactions on Scalable Information Systems (SIS). He was the program chair for Cluster’03, CCGrid'09, InfoScale’09, and ICPADS’09, ISPA’11, FCST’11, FutureTech’12, and Cluster2012; and the General Chair for IPDPS2012. He has also served as program committee members for numerous international conferences, including IPDPS, CCGrid, Cloud, CloudCom, Grid, HiPC, ICPP, and ICPADS. Professor Wang is the primary investigator of China 863 project 'Hong Kong University Grid Point' (2006-2011). The HKU Grid point consists of 3004 CPU cores (31.45 Teraflops), which offers parallel computing services for the China National Grid (CNGrid) and is used as a testbed for Cloud-related systems development. He has been invited to give keynote and plenary talk related to Distributed JVM design and Cloud Computing at various international conferences.",
         [['COMP3230', 'Operating Systems']],
         ['Cloud Computing']
     ))
 
     tutors.append(add_tutor(
-        'azero', 'azero', 'alpha_zero@deepmind.com', 'AlphaGo', 'Zero',
+        'azero', 'azero', 'alpha_zero@deepmind.com', 'AlphaGo', 'Zero', 'PT',
         np.iinfo(np.int32).max,
         r'I learn by meself so well. No human beats me.',
         [['COMP3314', 'Machine Learning']],
@@ -93,14 +103,19 @@ r"Professor Cho-Li Wang received his B.S. degree in Computer Science and Informa
     ))
 
     tutors.append(add_tutor(
-        'kpwat', 'kpwat', 'watkp@hku.hk', 'Kam Pui', 'Wat',
-        -1,
+        'kpwat', 'kpwat', 'watkp@hku.hk', 'Kam Pui', 'Wat', 'PT', -1,
         r'Dr. Wat receives a first class honour from BSc(Ac).',
         [['COMP2601', 'Probability & Statistics I']],
         ['Risk Management', 'Statistics']
     ))
 
     return tutors
+
+def populate_session(tutors):
+    if tutors is not None:
+        for tutor in tutors:
+            if tutor.tutor_type == 'CT':
+                pass
 
 
 def populate_student():
