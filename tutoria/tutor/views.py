3d0a5dd928c9701654e02646321122deed85ebe2
Jerome <zjrtomjohn@hotmail.com>
2017-10-28 22:34:02 +0800

    Improve confirm-booking page and add booking conditions.

--- tutoria/tutor/views.py
+++ tutoria/tutor/views.py
@@ -25,14 +25,18 @@ def book_session(request, tutor_id):
         username = request.session['username']
         if username is not None:  # If the user has logged in
             student = Student.objects.get(username=username)
-            # if (student.wallet_balance - tutor.hourly_rate)
-        session_id = request.POST.get('session_id', '')
-        tutor = Tutor.objects.get(pk=tutor_id)
-        session = Session.objects.get(pk=session_id)
-        return render(request, 'book.html', {'tutor': tutor,
-                                             'session': session})
-    else:
-        return HttpResponse("not a POST request!")
+            session_id = request.POST.get('session_id', '')
+            tutor = Tutor.objects.get(pk=tutor_id)
+            session = Session.objects.get(pk=session_id)
+            # Ignore commission for now because it might be saved by coupon
+            if (student.wallet_balance - tutor.hourly_rate) < 0:
+                return HttpResponse("You don't have enough money.")
+            if (tutor.username == student.username):
+                return HttpResponse("You can't book your session.")
+            if (student.bookingrecord_set.all().exists()):
+                return HttpResponse("You can only book one session per day!")
+            return render(request, 'book.html', {'tutor': tutor,
+                                                 'session': session})
 
 
 def save_booking(request, tutor_id):
