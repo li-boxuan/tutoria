0015edbfa706e2b70ce15acee4226e57b3cc0ff8
Jerome <zjrtomjohn@hotmail.com>
2017-10-28 16:56:11 +0800

    Receive POST message

--- tutoria/tutor/views.py
+++ tutoria/tutor/views.py
@@ -4,10 +4,10 @@ from django.shortcuts import (get_object_or_404, render, render_to_response,
                               redirect)
 from django.views import generic
 from django.template import RequestContext
-from .forms import BookForm
 from django.http import HttpResponse
 
 from account.models import Tutor
+from scheduler.models import Session
 
 
 def detail(request, tutor_id):
@@ -23,12 +23,11 @@ def detail(request, tutor_id):
 def book_session(request, tutor_id):
     """Book a new session."""
     if request.method == 'POST':
-        return HttpResponse("receive POST request!")
-        # if form.is_valid():
-        # booking = form.save(commit=False)
-        # # Save some other hidden attributes
-        # booking.save()
-        # return redirect('detail', tutor_id)
+        session_id = request.POST.get('session_id', '')
+        tutor = Tutor.objects.get(pk=tutor_id)
+        session = Session.objects.get(pk=session_id)
+        return HttpResponse(tutor.get_full_name() + " | session = "
+                            + session_id)
     else:
         return HttpResponse("In ELSE")
 # -----------------------------------------------------------------------------
