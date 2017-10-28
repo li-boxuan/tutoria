58081f3dd1dd958cdae5ec693ed379b387103305
liboxuanhk <liboxuanhk@gmail.com>
2017-10-28 20:29:33 +0800

    [dashboard] fix views

--- tutoria/dashboard/views.py
+++ tutoria/dashboard/views.py
@@ -4,7 +4,7 @@ from django.urls import reverse
 from django.views import generic
 
 from scheduler.models import BookingRecord
-
+from account.models import Student
 
 
 #def MybookingsView(request):
@@ -19,14 +19,15 @@ class MybookingsView(generic.ListView):
     model = BookingRecord
     template_name = 'my_bookings.html'
     context_object_name = 'my_booking_records'
-    
 
     def get_context_data(self, **kwargs):
         #context = super(MybookingsView,self).get_context_data(**kwargs)
         #context.objects.filter(user=self.request.user)
-	if 'username' not in self.request.session:
-	    return None
-	else:
-	    ursn=self.request.session['username']
-            context= BookingRecord.objects.filter(student_name==ursn)
-	    return context        
+        if 'username' not in self.request.session:
+            return None
+        else:
+            ursn=self.request.session['username']
+            urs = get_object_or_404(Student, username=ursn)
+            context=urs.bookingRecord_set.all
+            #context= BookingRecord.objects.filter(student.username==ursn)
+            return context        
