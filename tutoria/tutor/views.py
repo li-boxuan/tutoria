bf19322dea2f82e5e7cae60df62d612129bfd886
Jerome <zjrtomjohn@hotmail.com>
2017-10-28 22:00:35 +0800

    Finish beautify UI.

--- tutoria/tutor/views.py
+++ tutoria/tutor/views.py
@@ -2,12 +2,11 @@
 from __future__ import print_function
 from django.shortcuts import (get_object_or_404, render, render_to_response,
                               redirect)
-from django.views import generic
-from django.template import RequestContext
 from django.http import HttpResponse
 
-from account.models import Tutor
+from account.models import Tutor, Student
 from scheduler.models import Session
+from django.contrib.auth.decorators import login_required
 
 
 def detail(request, tutor_id):
@@ -19,9 +18,14 @@ def detail(request, tutor_id):
 # ####### Book Session #######
 
 
+@login_required
 def book_session(request, tutor_id):
     """Confirm booking a new session."""
     if request.method == 'POST':
+        username = request.session['username']
+        if username is not None:  # If the user has logged in
+            student = Student.objects.get(username=username)
+            if (student.wallet_balance - tutor.hourly_rate)
         session_id = request.POST.get('session_id', '')
         tutor = Tutor.objects.get(pk=tutor_id)
         session = Session.objects.get(pk=session_id)
