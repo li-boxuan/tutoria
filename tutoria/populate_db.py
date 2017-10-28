c7c0571cfc2fc91477150265d89b71d3a78c0da5
ZHANG Jiayao <zjohnson@connect.hku.hk>
2017-10-28 22:48:52 +0800

    updated schema and corresponding templates and populating methods

--- tutoria/populate_db.py
+++ tutoria/populate_db.py
@@ -21,15 +21,18 @@ OFFICE_HOUR_STEP = {'CT': timedelta(hours=0.5),
 
 def add_student(username, password, email, first_name, last_name,
                 wallet_balance=-1, avatar='default_avatar.png'):
-    from account.models import Student
+    from account.models import (User, Student)
     if wallet_balance < 0:
         wallet_balance = np.random.randint(1, 300) * 10
 
-    student = Student.objects.create_user(username, email, password,
+    user, _ = User.objects.get_or_create(username=username, email=email,
+									  password=password,
                                          first_name=first_name,
                                          last_name=last_name)
+    student, _ = Student.objects.get_or_create(user=user)
     student.wallet_balance = wallet_balance
     student.avatar = avatar
+    user.save()
     student.save()
     return student
 
@@ -41,7 +44,7 @@ def add_tutor(username, password, email, first_name, last_name,
                             tags=['Software Engineering'],
                           wallet_balance=-1, avatar='default_avatar.png',
                         sessions=None):
-    from account.models import (Tutor, Course, SubjectTag)
+    from account.models import (User, Tutor, Course, SubjectTag)
     if wallet_balance < 0:
         wallet_balance = np.random.randint(1, 300) * 10
     if tutor_type == 'CT':
@@ -49,9 +52,11 @@ def add_tutor(username, password, email, first_name, last_name,
     elif hourly_rate < 0:
         hourly_rate = np.random.randint(1, 300) * 10
 
-    tutor = Tutor.objects.create_user(username, email, password,
+    user, _ = User.objects.get_or_create(username=username, email=email,
+									  password=password,
                                          first_name=first_name,
                                          last_name=last_name)
+    tutor, _ = Tutor.objects.get_or_create(user=user)
     tutor.tutor_type = tutor_type
     tutor.wallet_balance = wallet_balance
     tutor.avatar = avatar
@@ -69,7 +74,7 @@ def add_tutor(username, password, email, first_name, last_name,
         for session in sessions:
             session.tutor = tutor
             session.save()
-            
+    user.save()
     tutor.save()
     return tutor
 
@@ -139,6 +144,9 @@ def populate_session(tutors):
 
 def populate_student():
     students = []
+    students.append(add_student(
+        'kpwat', 'kpwat', 'watkp@hku.hk', 'Kam Pui', 'Wat'
+    ))
     students.append(add_student(
         'ckchui', 'ckchui', 'ckchui@cs.hku.hk', 'Chun-Kit', 'Chui'
     ))
